82507866cea2749afcb5b06a291bbd9f957b6792
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-07-31 09:29:32 -0300

    1st commit

---
+++ sql/post_load.sql
@@ -0,0 +1,15 @@
+﻿-- cleanse 'ciências da natureza' scores.
+update raw_enem_scores set nature_sciences_score = trim(both ' ' from nature_sciences_score) where nature_sciences_score is not null;
+update raw_enem_scores set nature_sciences_score = null where nature_sciences_score = '.';
+
+-- cleanse 'ciências humanas' scores.
+update raw_enem_scores set human_sciences_score = trim(both ' ' from human_sciences_score) where human_sciences_score is not null;
+update raw_enem_scores set human_sciences_score = null where human_sciences_score = '.';
+
+-- cleanse 'linguagens e códigos' scores.
+update raw_enem_scores set languages_and_codes_score = trim(both ' ' from languages_and_codes_score) where languages_and_codes_score is not null;
+update raw_enem_scores set languages_and_codes_score = null where languages_and_codes_score = '.';
+
+-- cleanse 'matemática' scores.
+update raw_enem_scores set math_score = trim(both ' ' from math_score) where math_score is not null;
+update raw_enem_scores set math_score = null where math_score = '.';
---
+++ sql/run_aggregations.sql
@@ -0,0 +1,195 @@
+﻿-----------
+-- schools
+-----------
+
+-- ciencias da natureza
+insert into 
+  aggregated_scores_by_school
+select 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r 
+inner join 
+  schools s on s.id = r.school_id 
+inner join 
+  enem_subjects es on es.name = 'ciencias da natureza'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-- ciencias humanas
+insert into 
+  aggregated_scores_by_school
+select 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r 
+inner join 
+  schools s on s.id = r.school_id 
+inner join 
+  enem_subjects es on es.name = 'ciencias humanas'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-- linguagens e codigos
+insert into 
+  aggregated_scores_by_school
+select 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r 
+inner join 
+  schools s on s.id = r.school_id
+inner join 
+  enem_subjects es on es.name = 'linguagens e codigos'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  s.id, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+  
+-- matematica
+insert into 
+  aggregated_scores_by_school
+select 
+  s.id, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r 
+inner join 
+  schools s on s.id = r.school_id
+inner join 
+  enem_subjects es on es.name = 'matematica'
+where 
+  r.year = 2011 and r.math_score is not null 
+group by 
+  s.id, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-----------
+-- states
+-----------
+
+-- ciencias da natureza
+insert into 
+  aggregated_scores_by_state
+select 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'ciencias da natureza'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-- ciencias humanas
+insert into 
+  aggregated_scores_by_state
+select 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'ciencias humanas'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-- linguagens e codigos
+insert into 
+  aggregated_scores_by_state
+select 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'linguagens e codigos'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.state, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+  
+-- matematica
+insert into 
+  aggregated_scores_by_state
+select 
+  r.state, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'matematica'
+where 
+  r.year = 2011 and r.math_score is not null 
+group by 
+  r.state, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  4;
+
+-----------
+-- cities
+-----------
+
+-- ciencias da natureza
+insert into 
+  aggregated_scores_by_city
+select 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'ciencias da natureza'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  5;
+
+-- ciencias humanas
+insert into 
+  aggregated_scores_by_city
+select 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'ciencias humanas'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  5;
+
+-- linguagens e codigos
+insert into 
+  aggregated_scores_by_city
+select 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'linguagens e codigos'
+where 
+  r.year = 2011 and r.nature_sciences_score is not null 
+group by 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.nature_sciences_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  5;
+  
+-- matematica
+insert into 
+  aggregated_scores_by_city
+select 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1), count(*)
+from 
+  raw_enem_scores r inner join enem_subjects es on es.name = 'matematica'
+where 
+  r.year = 2011 and r.math_score is not null 
+group by 
+  r.city_id, r.city, r.year, es.id, trunc((cast(r.math_score as float) - 0.0000000001) / 100 + 1)
+order by 
+  5;
