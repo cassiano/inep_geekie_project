a6539a6d29e4a0fecc45c57c9349d84a7e42f52c
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-08-02 15:01:46 -0300

    Doing minor UX improvements

--- static/javascripts/autocomplete.js
+++ static/javascripts/autocomplete.js
@@ -1,31 +1,11 @@
 $(function() {
   var cache = { cities: {}, schools: {} };
   
-  $('#school').autocomplete({
-    minLength: 3,
-    source: function(request, response) {
-      var term = request.term;
-      if (term in cache.schools) {
-        response(cache.schools[term]);
-        return;
-      }
-
-      $.getJSON('/schools/search/' + window.viewModel.cityId() + '.json', request, function(data, status, xhr) {
-        cache.schools[term] = data.schools;
-        response(cache.schools[term]);
-      });
-    },
-    select: function(event, ui) {
-      // Update the view model with the selected school id and name.
-      window.viewModel.schoolId(ui.item.id);
-      window.viewModel.schoolName(ui.item.value);
-    }
-  });
-
   $('#city').autocomplete({
     minLength: 3,
     source: function(request, response) {
       var term = request.term;
+
       if (term in cache.cities) {
         response(cache.cities[term]);
         return;
@@ -40,8 +20,32 @@ $(function() {
       // Update the view model with the selected city id and name.
       window.viewModel.cityId(ui.item.id);
       window.viewModel.cityName(ui.item.value);
-      $('#school').val('');
-      $('#chartContainer').hide();
+      
+      $('#school-container').show();  // Show the school container (label + autocomplete).
+      $('#school').val('');           // Reset the school (autocomplete) input text.
+      $('#chartContainer').hide();    // Hide the chart.
+    }
+  });
+
+  $('#school').autocomplete({
+    minLength: 3,
+    source: function(request, response) {
+      var term = request.term;
+
+      if (term in cache.schools) {
+        response(cache.schools[term]);
+        return;
+      }
+
+      $.getJSON('/schools/search/' + window.viewModel.cityId() + '.json', request, function(data, status, xhr) {
+        cache.schools[term] = data.schools;
+        response(cache.schools[term]);
+      });
+    },
+    select: function(event, ui) {
+      // Update the view model with the selected school id and name.
+      window.viewModel.schoolId(ui.item.id);
+      window.viewModel.schoolName(ui.item.value);
     }
   });
 });
